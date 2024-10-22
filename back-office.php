bd8529c1fdd25c97c5bfc6e27defce78dbd702e8
loanb <loanboulin.pro@gmail.com>
2024-10-22 11:20:14 +0200

    feat : page backoffice

---
+++ back-office.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <link rel="stylesheet" href="style/style.css"/>
+    <title>BACKOFFICE</title>
+</head>
+<body>
+
+<h1>Accueil</h1>
+
+<h2>
+    Liste des réservations
+</h2>
+
+<?php
+    // Inclure le fichier reservations.php
+    include 'components/reservations_list.php';
+?>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,62 +1,19 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css"/>
     <title>Accueil</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 20px;
-        }
-        table {
-            width: 100%;
-            border-collapse: collapse;
-        }
-        th, td {
-            border: 1px solid #ddd;
-            padding: 8px;
-            text-align: left;
-        }
-        th {
-            background-color: #f2f2f2;
-        }
-        .button {
-            padding: 10px 15px;
-            margin-left: 20px;
-            background-color: #007BFF;
-            color: white;
-            border: none;
-            border-radius: 5px;
-            text-decoration: none;
-            font-size: 16px;
-        }
-        .button:hover {
-            background-color: #0056b3;
-        }
-    </style>
+    
 </head>
 <body>
 
-<h1>Accueil</h1>
-
-<h2>
-    Liste des réservations
-    <a href="components/form.php" class="button">Ajouter une réservation</a>
-</h2>
-
-<?php
-    // Inclure le fichier reservations.php
-    include 'components/reservations_list.php';
-?>
-
+<h1>Accueil<a href="components/form.php" class="button">Ajouter une réservation</a></h1> 
 
 <h2>
     Liste des playlists
 </h2>
 
 <?php
-    // Inclure le fichier reservations.php
     include 'components/playlists_list.php';
 ?>
 
